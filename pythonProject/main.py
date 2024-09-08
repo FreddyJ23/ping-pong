7ef81de8b21aa4d3f9549344d1e3a74e379425a6
freddy <freedomochulor22gmail.com>
2024-09-08 23:56:55 +0100

    project done

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -34,7 +34,7 @@ while game_is_on:
     if ball.ycor() > 280 or ball.ycor() < -280:
         #Bounce from wall
         ball.y_bounce()
-    if ball.distance(r_paddle) < 50 and ball.xcor() == 330 or ball.distance(l_paddle) < 50 and ball.xcor() == -330:
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         # Bounce from paddle
         ball.x_bounce()
     # Detect past R paddle
